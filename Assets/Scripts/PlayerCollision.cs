96317813f849556b3995fe820bfe1c27fc86270e
Mark Anthony Viola <mark.anthony.viola@gmail.com>
2017-09-30 13:27:21 -0400

    Added player movement and object collision

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("Hit something!");
+        if(collision.collider.tag == "Obstacle")
+        {
+            Debug.Log("Hit obstacle!");
+            movement.enabled = false;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("In Start\n");
+        //rb.AddForce(0, 200, 500);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //rb.AddForce(0, 0, 2000*Time.deltaTime);
+	}
+
+    // Use for Unity physics
+    private void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
